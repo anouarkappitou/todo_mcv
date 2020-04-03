84880bafcd9d206f8f31f95806d194ecb3c2eaf1
Kappitou Anouar <anourkappitou@gmail.com>
2020-04-03 21:47:04 +0100

    added initial test

---
+++ cypress/intergration/app_spec.js
@@ -0,0 +1,8 @@
+
+describe('Context', function() {
+
+	  it('true for sure should be equal to true but who knows this is javascript you need to double check :D', function() {
+		      expect(true).to.equal(true)
+	})
+
+})
