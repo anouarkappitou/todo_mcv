924c7ffa2c605ca6a6aafee22e48c4d09d2672cd
KAPPITOU Anouar <anoirkapp@gmail.com>
2020-04-05 19:29:52 +0100

    chores: refactoring test and grouping them by context

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,167 +1,169 @@
 describe('Todo Application', function() {
 
-	let wish_list = [
-		"TODO 1",
-		"TODO 2",
-		"TODO 3",
-		"TODO 4",
-		"TODO 5",
-		"TODO 6",
-		"TODO 7",
-		"TODO 8",
-		"TODO 9",
-		"TODO 10",
-	];
+        let wish_list = [
+                "TODO 1",
+                "TODO 2",
+                "TODO 3",
+                "TODO 4",
+                "TODO 5",
+                "TODO 6",
+                "TODO 7",
+                "TODO 8",
+                "TODO 9",
+                "TODO 10",
+        ];
 
 
-	let seed = () => {
+        let seed = () => {
 
-		let input = cy.get(".new-todo");
+                let input = cy.get(".new-todo");
 
-		wish_list.forEach(( element ) => {
-			input.type( element + " {enter}");
-		});
+                wish_list.forEach(( element ) => {
+                        input.type( element + " {enter}");
+                });
 
-	}
-
-
-	let random_array = ( len ) => {
-		let array = [...Array(len + 1 ).keys()];
-
-		// remove 0 element
-		array.splice(0,1);
-
-		var currentIndex = array.length, temporaryValue, randomIndex;
-
-		  while (0 !== currentIndex) {
-		
-		    randomIndex = Math.floor(Math.random() * currentIndex);
-		    currentIndex -= 1;
-
-		    temporaryValue = array[currentIndex];
-		    array[currentIndex] = array[randomIndex];
-		    array[randomIndex] = temporaryValue;
-		  }
-
-		  return array;
-	}
+        }
 
 
-	let seed_and_check = ( checked_len ) => {
+        let random_array = ( len ) => {
+                let array = [...Array(len + 1 ).keys()];
 
-		seed();
+                // remove 0 element
+                array.splice(0,1);
 
-		random_array(checked_len)
-			.forEach( (item) => {
-				cy.get("li .toggle").eq(item).check();
-				cy.wait( 100 );
-		});
+                var currentIndex = array.length, temporaryValue, randomIndex;
 
-	}
+                while (0 !== currentIndex) {
 
+                        randomIndex = Math.floor(Math.random() * currentIndex);
+                        currentIndex -= 1;
 
-	let test_suits = {
+                        temporaryValue = array[currentIndex];
+                        array[currentIndex] = array[randomIndex];
+                        array[randomIndex] = temporaryValue;
+                }
 
-		should_add_one: () => {
-			cy.get(".new-todo")
-				.type(wish_list[0] + "{enter}")
-
-			cy.get(".todo-list")
-				.children().should('have.length', 1)
-
-		},
-		should_have_right_content: () => {
-			cy.get(".new-todo")
-				.type(wish_list[0] + "{enter}")
-
-			cy.get(".todo-list")
-				.children().should('have.length', 1)
+                return array;
+        }
 
 
-			cy.get(".todo-list label").eq(0).should('contain', wish_list[0]);
+        let seed_and_check = ( checked_len ) => {
 
-		},
-		should_delete_one: () => {
-			seed();
+                seed();
 
-			cy.get(".destroy").eq(0).click({force: true});
+                random_array(checked_len)
+                        .forEach( (item) => {
+                                cy.get("li .toggle").eq(item).check();
+                                cy.wait( 100 );
+                        });
 
-			cy.get(".todo-list")
-				.children().should('have.length', 9)
+        }
 
-			cy.get(".todo-list label").eq(0).should('not.contain', 'TODO 1');
+        context("adding items" , () => {
 
-		},
-		should_delete_all: () => {
-			seed();
+                it("should add new item" , () => {
+                        cy.get(".new-todo")
+                                .type(wish_list[0] + "{enter}")
 
-			for( var i = 0; i < wish_list.length; i++ )
-			{
-				cy.get('.todo-list .destroy').eq(0)
-				.invoke("show")
-				.click();
-				cy.wait( 100 );
-			}
+                        cy.get(".todo-list")
+                                .children().should('have.length', 1)
 
-			cy.get(".todo-list")
-				.children().should("have.length", 0 );
+                });
+                it("should have the right content", () => {
+                        cy.get(".new-todo")
+                                .type(wish_list[0] + "{enter}")
 
-
-		},
-		should_show_completed: () => {
-			seed_and_check(4);
-
-			cy.get('[href="#/completed"]')
-				.dblclick();
-
-			cy.get(".todo-list")
-				.children().should("have.length", 4 );
-		},
-		should_show_active: () => {
-			seed_and_check(6);
-
-			cy.get('[href="#/active"]')
-				.dblclick();
-
-			cy.get(".todo-list")
-				.children().should("have.length", 4 );
-
-		},
-		should_show_all: () => {
-			seed_and_check(2);
-
-			cy.get('[href="#/all"]')
-				.dblclick();
-
-			cy.get(".todo-list")
-				.children().should("have.length", 10 );
+                        cy.get(".todo-list")
+                                .children().should('have.length', 1)
 
 
-		},
-		should_clear_completed: () => {
-			seed_and_check(8);
+                        cy.get(".todo-list label").eq(0).should('contain', wish_list[0]);
+
+                });
+        });
+
+        context("deleting items", () => {
+                it("should delete one item ", () => {
+                        seed();
+
+                        cy.get(".destroy").eq(0).click({force: true});
+
+                        cy.get(".todo-list")
+                                .children().should('have.length', 9)
+
+                        cy.get(".todo-list label").eq(0).should('not.contain', wish_list[0]);
+
+                });
+                it("delete all items",() => {
+                        seed();
+
+                        for( var i = 0; i < wish_list.length; i++ )
+                        {
+                                cy.get('.todo-list .destroy').eq(0)
+                                        .invoke("show")
+                                        .click();
+                                cy.wait( 100 );
+                        }
+
+                        cy.get(".todo-list")
+                                .children().should("have.length", 0 );
 
 
-			cy.get(".clear-completed")
-				.dblclick();
-
-			cy.get('[href="#/completed"]')
-				.dblclick();
+                });
 
 
-			cy.get(".todo-list")
-				.children().should("have.length", 0 );
+                it("delete completed items", () => {
+                        seed_and_check(8);
 
-		}
-	}
 
-	it("should add new item" , test_suits.should_add_one );
-	it("should have the right content", test_suits.should_have_right_content );
-	it("should delete one item ", test_suits.should_delete_one );
-	it("delete all items", test_suits.should_delete_all );
-	it("show by completed", test_suits.should_show_completed );
-	it("show by active", test_suits.should_show_active );
-	it("show all", test_suits.should_show_all );
-	it("delete completed items", test_suits.should_clear_completed );
+                        cy.get(".clear-completed")
+                                .dblclick();
+
+                        cy.get('[href="#/completed"]')
+                                .dblclick();
+
+
+                        cy.get(".todo-list")
+                                .children().should("have.length", 0 );
+
+                });
+
+        });
+
+
+
+        context("filtering items", () => {
+                it("show by completed",() => {
+                        seed_and_check(4);
+
+                        cy.get('[href="#/completed"]')
+                                .dblclick();
+
+                        cy.get(".todo-list")
+                                .children().should("have.length", 4 );
+                });
+                it("show by active",() => {
+                        seed_and_check(6);
+
+                        cy.get('[href="#/active"]')
+                                .dblclick();
+
+                        cy.get(".todo-list")
+                                .children().should("have.length", 4 );
+
+                });
+                it("show all",() => {
+                        seed_and_check(2);
+
+                        cy.get('[href="#/all"]')
+                                .dblclick();
+
+                        cy.get(".todo-list")
+                                .children().should("have.length", 10 );
+
+
+                });
+        });
+
 
 })
